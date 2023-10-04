ec8ba1e2bc3e34769663a1ddf21849b14a8f8711
Shubham Mojidra <95879466+HuNt07@users.noreply.github.com>
2023-10-04 19:37:28 +0000

    Co-authored-by: sokumon <sokumon@users.noreply.github.com>

---
+++ server.mjs
@@ -0,0 +1,42 @@
+// Import required modules
+import express from 'express';
+import path from 'path';
+import {doZeroShot} from './zeroshot.js'
+import { fileURLToPath } from 'url'; 
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+// Create an Express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+
+// // Define the static file directory
+// const publicDirectoryPath = path.join(__dirname, 'public');
+
+// // Serve static files (e.g., index.html)
+// app.use(express.static(publicDirectoryPath));
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+app.post('/feeling',function(req, res) {
+  console.log(req.body.userinput)
+  doZeroShot(req.body.userinput).then((data)=>{
+    console.log('data is '+data)
+    let result = {
+      "feeling":data
+    }
+    console.log(result)
+    res.send(result)
+  })
+
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ zeroshot.js
@@ -0,0 +1,50 @@
+import { HfInference } from '@huggingface/inference'
+import dotenv from "dotenv";
+import { json } from 'stream/consumers';
+
+dotenv.config()
+
+const TOKEN = process.env.API   
+const inference = new HfInference(TOKEN)
+
+// function to merge key, value and make an object
+function convertToObj(labels, ans) {   
+  let obj = {};
+  labels.forEach((k, i) => 
+            { obj[k] = ans[i] })
+  return obj; 
+}
+// function to map values 
+function getObjectKey(obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value);
+}
+
+async function doZeroShot(input){
+  let prediction = await inference.zeroShotClassification({
+    model: 'facebook/bart-large-mnli',
+    inputs: [
+      input
+    ],
+    parameters: { candidate_labels: ['sad', 'happy', 'neutral', 'blush', 'cry'] }
+    
+})
+
+let test = prediction[0];
+let labels = test["labels"];
+let ans = test["scores"];
+ 
+
+let finals = convertToObj(labels, ans)
+
+// console.log(finals)
+
+let arr = Object.values(finals);
+let max = Math.max(...arr);
+
+
+
+console.log(getObjectKey(finals, max));
+return getObjectKey(finals,max)
+}
+
+export {doZeroShot}
